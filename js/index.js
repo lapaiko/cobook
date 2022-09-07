33833d22fffa11b9eb117a55a9a14ba601571130
mmspro <serega.lapayko@gmail.com>
2022-09-07 17:40:12 +0300

    close project

---
+++ js/index.js
@@ -0,0 +1,153 @@
+var chLesson = {
+	'uklen': {
+		'lesson': 'Українська мова',
+		'teacher': 'Волк Світлана Віталіївна',
+		'classroom': 'https://classroom.google.com/c/NTQ1MjMyODExMjEy',
+		'meet': 'https://meet.google.com/qte-iqxy-dcs'
+	},
+	'uklit': {
+		'lesson': 'Українська мова',
+		'teacher': 'Волк Світлана Віталіївна',
+		'classroom': 'https://classroom.google.com/c/NTQ1MjMzNDAzMDE0',
+		'meet': 'https://meet.google.com/zrm-ipgh-oxb'
+	},
+	'zarlit': {
+		'lesson': 'Зарубіжна література',
+		'teacher': 'Солодка Ірина Петрівна',
+		'classroom': 'https://classroom.google.com/c/NTQ0NzUwODYxNzA1',
+		'meet': 'https://meet.google.com/try-xbxe-eax'
+	},
+	'eng': {
+		'lesson': 'Англійська мова',
+		'teacher': 'Задоянчук Оксана Василівна',
+		'classroom': 'https://classroom.google.com/c/NTQ1NTY3Mjk4ODQ5',
+		'meet': 'https://meet.google.com/zvd-crbk-vgb'
+	},
+	'mat': {
+		'lesson': 'Математика',
+		'teacher': 'Карабаджак Ірина Анатоліївна',
+		'classroom': 'https://classroom.google.com/c/NTI2OTIzMTIyMzUz',
+		'meet': 'https://meet.google.com/wwy-pvek-fpm'
+	},
+	'mus': {
+		'lesson': 'Музичне мистецтво',
+		'teacher': 'Кериченко Наталія Миколаївна',
+		'classroom': 'https://classroom.google.com/c/NTI2OTIxMzY1NjM3',
+		'meet': 'https://meet.google.com/nfa-usas'
+	},
+	'hist': {
+		'lesson': 'Історія',
+		'teacher': 'Дідур Оксана Володимирівна',
+		'classroom': 'https://classroom.google.com/c/NTQ0NzkyOTQ2MTgw',
+		'meet': 'https://meet.google.com/gkd-tmfp-gfs'
+	},
+	'fiz': {
+		'lesson': 'Фізична культура',
+		'teacher': 'Стеблюк Олександра Петрівна',
+		'classroom': 'https://classroom.google.com/c/NTI3MjA4NzU0NjU1',
+		'meet': 'https://meet.google.com/zhz-eesi-pif'
+	},
+	'pain': {
+		'lesson': 'Образотворче мистецтво',
+		'teacher': 'Шклярук Світлана Олександрівна',
+		'classroom': 'https://classroom.google.com/c/NTI3MTc4MTYzNjQ5',
+		'meet': 'https://meet.google.com/cny-cwqt-dxb'
+	},
+	'prog': {
+		'lesson': 'Інформатика',
+		'teacher': 'Шевчук Андрій Григорович',
+		'classroom': 'https://classroom.google.com/c/NTQ1MTU5NzIwMDMx',
+		'meet': 'https://meet.google.com/tvf-poiw-qym'
+	},
+	'dram': {
+		'lesson': 'Драматургія і театр',
+		'teacher': 'Рахнянська Наталія Федорівна',
+		'classroom': 'https://classroom.google.com/c/NTI3MTI3NTg1MjMx',
+		'meet': 'https://meet.google.com/knk-wynn-myc'
+	},
+	'oz': {
+		'lesson': 'Основи здоровя',
+		'teacher': 'Щепанська Алла Борисівна',
+		'classroom': 'https://classroom.google.com/c/NTQ1MDE1NTYzNDM2',
+		'meet': '#'
+	},
+	'tec': {
+		'lesson': 'Технологія',
+		'teacher': 'Іванова Алла Опанасівна',
+		'classroom': 'https://classroom.google.com/c/NTQ1NTc3MDc3MTYz',
+		'meet': '#'
+	},
+	'nat': {
+		'lesson': 'Пізнаємо природу',
+		'teacher': 'Касяненко Світлана Володимирівна',
+		'classroom': 'https://classroom.google.com/c/NTQ1MDc1MDgxMzg1',
+		'meet': 'https://meet.google.com/hfo-mkni-seh'
+	}
+};
+
+var arSсhedule = [
+	['uklen', 'uklen', 'zarlit', 'eng', 'mat', 'mus'],
+	['eng', 'hist', 'mat', 'nat', 'uklit', 'fiz'],
+	['fiz', 'pain', 'prog', 'uklen', 'dram', 'mat'],
+	['fiz', 'nat', 'mat', 'uklen', 'zarlit', 'eng', 'oz'],
+	['mat', 'tec', 'tec', 'hist', 'uklit', 'eng']
+];
+
+var arWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четверг', 'Пятниця'];
+
+function CardDay(day, cardslesson) {
+	let cardday = '';
+	cardday = cardday + '<div class="shedule__days">';
+	cardday = cardday + '<div class="shedule__title title">';
+	cardday = cardday + '<h2>' + day + '</h2>';
+	cardday = cardday + '</div>';
+	cardday = cardday + '<div class="shedule__card">';
+
+	cardday = cardday + cardslesson;
+
+	cardday = cardday + '</div>';
+	cardday = cardday + '</div>';
+	return cardday;
+}
+
+function CardLesson(num, lesson, teacher, classroom, meet) {
+	var cardslesson = '';
+
+	cardslesson = cardslesson + '<div class="lesson">';
+	cardslesson = cardslesson + '<div class="lesson__number">';
+	cardslesson = cardslesson + '<p>' + num + '</p>';
+	cardslesson = cardslesson + '</div>';
+	cardslesson = cardslesson + '<div class="lesson__caption">';
+	cardslesson = cardslesson + '<div class="lesson__caption_name">' + lesson + '</div>';
+	cardslesson = cardslesson + '<div class="lesson__caption_teacher">' + teacher + '</div>';
+	cardslesson = cardslesson + '</div>';
+	cardslesson = cardslesson + '<div class="lesson__link">';
+	cardslesson = cardslesson + '<div class="lesson__link_classroom"><a href="' + classroom + '"';
+	cardslesson = cardslesson + 'target="_blank"><img src="img/classroom.png" alt=""></a></div>';
+	cardslesson = cardslesson + '<div class="lesson__link_meet"><a href="' + meet + '"';
+	cardslesson = cardslesson + 'target="_blank"><img src="img/meet.png" alt=""></a></div>';
+	cardslesson = cardslesson + '</div>';
+	cardslesson = cardslesson + '</div>';
+	return cardslesson;
+}
+
+function Shedule() {
+	//	for (let idU in chPanda.chName)
+	let shedule = '', lessons = '';
+	// Формуємо карточки за весь тиждень
+	for (let d = 0; d < arSсhedule.length; d++) {
+		lessons = '';
+		// Формуємо карточку уроків одного дня
+		for (let l = 0; l < arSсhedule[d].length; l++) {
+			let num = l + 1, idLesson = arSсhedule[d][l], Lesson = chLesson[idLesson];
+			let lesson = Lesson.lesson, teacher = Lesson.teacher, classroom = Lesson.classroom, meet = Lesson.meet;
+			lessons = lessons + CardLesson(num, lesson, teacher, classroom, meet);
+		}
+		// Формуємо карточку одного дня
+		nmWeek = arWeek[d];
+		shedule = shedule + CardDay(nmWeek, lessons);
+	}
+	document.getElementById('shedule').innerHTML = shedule;
+}
+
+Shedule();
